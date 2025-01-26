458a7e689c326b062cf86fc4666c41cd28915218
MountainBean <samueld.work@gmail.com>
2025-01-26 17:26:09 +1300

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sys/socket.h>
+
+int start_server(int my_port) {
+    socket(AF_INET, );
+    return 0;
+};
+
+int main(void) {
+    std::cout << "Hello, world!" << std::endl;
+}
+
