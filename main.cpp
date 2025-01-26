24cbbafbb8ea295c79b95b8fbe5d72de1d72103a
MountainBean <samueld.work@gmail.com>
2025-01-27 10:55:45 +1300

    added bind, listen

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
+#include <cstring>
 #include <iostream>
+#include <arpa/inet.h>
+#include <linux/in.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 
+
 int start_server(int my_port) {
-    socket(AF_INET, );
+    struct in_addr all_addr;
+    struct sockaddr_in my_sockaddr;
+
+    inet_aton("0.0.0.0", &all_addr);
+    memset(&my_sockaddr, 0, sizeof(my_sockaddr));
+    my_sockaddr.sin_family = AF_INET;
+    my_sockaddr.sin_port = htons(8080);
+    my_sockaddr.sin_addr = all_addr;
+
+    // syscall socket()
+    int sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    // syscall bind()
+    if (bind(sock_fd, (struct sockaddr *) &my_sockaddr, sizeof(my_sockaddr)) != 0) {
+        std::cout << "bind failed" << std::endl;
+    };
+    // syscall listen()
+    if (listen(sock_fd, 0) != 0) {
+        std::cout << "listen failed" << std::endl;
+    }
+
     return 0;
 };
 
