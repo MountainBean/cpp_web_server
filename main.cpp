52d36c29dcc2628ea55dcfe72367c6b2afb0c7a6
MountainBean <samueld.work@gmail.com>
2025-02-14 11:08:18 +1300

    replaced C-style casts with static_cast except for the struct ipaddr_in *

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include <cstdint>
 #include <cstdlib>
 #include <format>
-#include <cstring>
 #include <iostream>
 #include <arpa/inet.h>
 #include <linux/in.h>
 #include <netinet/in.h>
-#include <string>
+#include <string_view>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 //! begins listening on my_port
 int start_server(std::uint16_t my_port) {
-    struct sockaddr_in my_sockaddr;
-    memset(&my_sockaddr, 0, sizeof(my_sockaddr));           // Zero out the struct memory
+    struct sockaddr_in my_sockaddr {};
     my_sockaddr.sin_family = AF_INET;
     my_sockaddr.sin_port = htons(my_port);
-    my_sockaddr.sin_addr = (struct in_addr) INADDR_ANY;     // 0.0.0.0
+    my_sockaddr.sin_addr = static_cast<struct in_addr>(INADDR_ANY);     // 0.0.0.0
 
     // syscall socket()
     int sock_fd {socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)};
     // syscall bind()
+    // WARNING: Move away from C-style cast here. static cast won't work
     if (bind(sock_fd, (struct sockaddr *) &my_sockaddr, sizeof(my_sockaddr)) != 0) {
         std::cout << "bind failed" << std::endl;
         return -1;
@@ -38,10 +37,10 @@ int start_server(std::uint16_t my_port) {
 // child process handle function
 int handle_request(int clientfd) {
     char buf[1024];
-    std::string getReq { "GET " };
+    constexpr std::string_view getReq { "GET " };
 
     long read_length {read(clientfd, &buf, sizeof(buf))};
-    std::string req { buf };
+    std::string_view req { buf };
     if (req.substr(0, 4) == getReq) {
 
         write(4, "HTTP/1.1 200 OK\r\n\r\n", 19);
@@ -61,7 +60,7 @@ int main(int argc, char* argv[]) {
             std::cout << "Given port must be between 1024 and 60999\n";
             return -1;
         }
-        my_port = (std::uint16_t) my_long_port;
+        my_port = static_cast<std::uint16_t>(my_long_port);
     }
     std::cout << std::format("Starting server on port {}\n", my_port);
     int sock_fd = {start_server(my_port)};
